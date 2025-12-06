4a00fb2736b94df6251097f8a6bb96ddc0a0e364
ohidurgclan <ohidur.ru.2019@gmail.com>
2025-12-07 04:15:16 +0600

    CLP Update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId  } = require('mongodb');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -24,18 +24,28 @@ const client = new MongoClient(URI, {
 });
 
 let userCollection;
+let menuCollection;
+let kitchenCollection;
 
 // Connect only once
 async function connectDB() {
-  if (!userCollection) {
+  if (client.topology && client.topology.isConnected()) return;
+  try {
     await client.connect();
-    const database = client.db("amarKitchen");
-    userCollection = database.collection("users");
+    const db = client.db("amarKitchen");
+    userCollection = db.collection("users");
+    menuCollection = db.collection("menu");
+    kitchenCollection = db.collection("kitchen");
+
     console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err.message);
+    process.exit(1);
   }
 }
-
-// ------------------ ROUTES ------------------
+// -----------------------------------------------------------------------------
+// ------------------------------- ROUTES STARTS -------------------------------
+// -----------------------------------------------------------------------------
 
 app.get("/users", async (req, res) => {
   await connectDB();
@@ -46,6 +56,28 @@ app.get("/users", async (req, res) => {
 app.post("/users", async (req, res) => {
   await connectDB();
   const result = await userCollection.insertOne(req.body);
+  res.json(result);
+});
+
+app.put("/users/:id", async (req, res) => {
+  await connectDB();
+  const id = req.params.id;
+  const updatedUser = {
+    $set: req.body
+  };
+  const result = await userCollection.updateOne(
+    { _id: new ObjectId(id) },
+    updatedUser
+  );
+  res.json(result);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  await connectDB();
+  const id = req.params.id;
+  const result = await userCollection.deleteOne({
+    _id: new ObjectId(id)
+  });
   res.json(result);
 });
 
@@ -63,23 +95,21 @@ app.post("/login", async (req, res) => {
   await connectDB();
   const { email, password } = req.body;
   const user = await userCollection.findOne({ email });
-
   if (!user) return res.status(404).json({ message: "User not found" });
-
   const correct = await bcrypt.compare(password, user.password);
   if (!correct) return res.status(400).json({ message: "Invalid password" });
-
   const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "1d" });
-
   res.json({ success: true, token });
 });
-
+// -----------------------------------------------------------------------------
+// ------------------------------- ROUTES ENDS ---------------------------------
+// -----------------------------------------------------------------------------
 // Root Domain Route
 app.get("/", (req, res) => {
   res.send("Congrats Md Ohidur Rahman (S_ID: 221002406)! Your AmarKitchen Production Server is Running!...");
 });
 
-// Logical Set For Proiduction and Local Environment
+// Logical Setup For Proiduction &  Local Environment
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 7090;
   app.listen(PORT, () => {
